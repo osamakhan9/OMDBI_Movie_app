026049f3dc996d1c8b1a7017831f385ebabb4761
mohammad osama <okosama06@gmail.com>
2023-11-07 09:39:46 +0530

    backend is complete

---
+++ backend/index.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const axios = require('axios');
+const cors = require("cors");
+require("dotenv").config();
+const app = express();
+app.use(cors());
+
+const port = process.env.PORT || 8000;
+app.get('/api/search', async (req, res) => {
+  const { query,page } = req.query; // Change this line to use req.query
+  console.log(query);
+  try {
+    const response = await axios.get(`http://www.omdbapi.com/?s=${query}&apikey=${process.env.API_KEY}&page=${page}`);
+    res.json(response.data);
+  } catch (error) {
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+app.get('/api/filter', async (req, res) => {
+  try {
+    const { year } = req.query;
+    const response = await axios.get(`http://www.omdbapi.com/?y=${year}&s=movie&apikey=${process.env.API_KEY}`);
+    res.json(response.data);
+  } catch (error) {
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
